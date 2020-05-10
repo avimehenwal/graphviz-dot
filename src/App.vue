3fa18fb14cd484a49d258014002135750e74586d
avimehenwal <avi.mehanwal@gmail.com>
2020-05-11 00:32:46 +0200

    test: :green_heart: Dot language component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <script src="https://unpkg.com/@hpcc-js/wasm/dist/index.min.js" type="javascript/worker"></script>
-    <GraphvizViewer />
+    <DotLanguage
+      code="digraph {
+        node [fontsize=24, colorscheme=set312, style=filled]
+        a [fillcolor=1]
+        b [fillcolor=2]
+        c [fillcolor=3]
+        a -> b -> c
+      }
+      "
+    />
   </div>
 </template>
 
 <script>
-import GraphvizViewer from './components/GraphvizViewer.vue';
+import DotLanguage from './components/DotLanguage.vue';
 
 export default {
   name: 'home',
   components: {
-    GraphvizViewer,
+    DotLanguage,
   },
 
 };
--- src/components/DotLanguage.vue
+++ src/components/DotLanguage.vue
@@ -10,11 +10,14 @@ import * as d3 from 'd3';
 import 'd3-graphviz';
 
 export default {
-  data() {
-    return {
-      code: 'digraph {a -> b}'
-    };
-  },
+  props: [
+    'code'
+  ],
+  // data() {
+  //   return {
+  //     code: 'digraph {a -> b}'
+  //   };
+  // },
   mounted() {
     console.log(d3.select('#graph').graphviz("#graph").renderDot(this.code))
     d3.select('#graph').graphviz("#graph").renderDot(this.code);
