791650d3b5d1945958d71c5b708fb00cb64b52b5
avimehenwal <avi.mehanwal@gmail.com>
2020-07-10 03:39:19 +0200

    feat(component): :chart_with_upwards_trend: d3-graphviz component

---
+++ graphvizEg/components/global/Dot.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <script src="https://unpkg.com/@hpcc-js/wasm/dist/index.min.js" type="javascript/worker" />
+    <h3>from Component</h3>
+    <div id="graph" />
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+import 'd3-graphviz'
+
+export default {
+  props: {
+    code: {
+      type: String,
+      default: 'digraph {a -> b}'
+    }
+  },
+  mounted () {
+    d3.select('#graph').graphviz('#graph').renderDot(this.code)
+  }
+}
+</script>
