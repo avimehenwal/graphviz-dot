e696cdf4de0f73d55ad4108ee9b8590eacdb39b0
avimehenwal <avi.mehanwal@gmail.com>
2020-07-10 02:59:38 +0200

    refactor(component): :sparkles: Add types to props

--- src/components/DotLanguage.vue
+++ src/components/DotLanguage.vue
@@ -2,26 +2,23 @@
   <div>
     <script src="https://unpkg.com/@hpcc-js/wasm/dist/index.min.js" type="javascript/worker" />
     <h3>from Component</h3>
-    <div id="graph"></div>
+    <div id="graph" />
   </div>
 </template>
 
 <script>
-import * as d3 from 'd3';
-import 'd3-graphviz';
+import * as d3 from 'd3'
+import 'd3-graphviz'
 
 export default {
-  props: [
-    'code'
-  ],
-  // data() {
-  //   return {
-  //     code: 'digraph {a -> b}'
-  //   };
-  // },
-  mounted() {
-    console.log(d3.select('#graph').graphviz("#graph").renderDot(this.code))
-    d3.select('#graph').graphviz("#graph").renderDot(this.code);
+  props: {
+    code: {
+      type: String,
+      default: 'digraph {a -> b}'
+    }
+  },
+  mounted () {
+    d3.select('#graph').graphviz('#graph').renderDot(this.code)
   }
-};
+}
 </script>
